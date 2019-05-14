c616a36bee49c86608a210751a4753370024c114
Max Base <MaxBaseCode@Gmail.Com>
2019-05-14 12:30:57 +0430

    Update google-magic.py

--- google-magic.py
+++ google-magic.py
@@ -1,1 +1,20 @@
+import os
+import sys
+import time
+import random
+import pprint
+import codecs
 
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+from magic_google import MagicGoogle
+
+mg = MagicGoogle()
+
+x=0
+file = codecs.open("result-"+ str(x) + ".txt", "w", "utf-8")
+for url in mg.search_url(query='Github',num=100, start=x * 100):
+	file.write(url)
+	print(url)
+	# file.write("\n")
+	file.write("\n----------------\n")
+file.close()

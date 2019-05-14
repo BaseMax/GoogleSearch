6f83a0eb2e357fce4923edb7ed55723ca96f89bc
Max Base <MaxBaseCode@Gmail.Com>
2019-05-14 11:42:13 +0430

    Update google-gsearch.py

--- google-gsearch.py
+++ google-gsearch.py
@@ -1,3 +1,4 @@
+import codecs
 from gsearch.googlesearch import search
 
 # results = search('Github')  
@@ -5,5 +6,21 @@ results = search('Github', num_results=100)
 
 # print(results)
 
+file = codecs.open("result.txt", "w", "utf-8")
+
 for item in results:
-	print(item)
+
+	# It will not works on windows OS : utf8 problem...
+	# https://stackoverflow.com/questions/934160/write-to-utf-8-file-in-python
+	# print(item)
+	# print(item[0])
+	# print(item[1])
+	# print("----------------")
+
+	file.write(item[0])
+	file.write("\n")
+	file.write(item[1])
+	file.write("\n----------------\n")
+
+file.close()
+
